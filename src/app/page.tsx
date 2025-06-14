664f4c9a7dad200a9f7a6bd6d4feaa83ee37de49
mefarran2 <mefarran2@vt.edu>
2025-06-14 16:43:40 -0400

    working on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-import Image from "next/image";
+import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 
 export default function Home() {
   return (
-    <Container maxWidth="lg">
-
+    <Container maxWidth="xl">
+      <Box marginTop={5}>
+        <Grid container spacing={5}>
+          <Grid size={6} >
+            <Paper elevation={4} sx={{height: 500}}>
+              <Typography sx={{fill: 'true'}}>paper</Typography>
+            </Paper>
+          </Grid>
+          <Grid size={6} >
+            <Paper elevation={4}>
+              <Typography>paper</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Box>
     </Container>
   );
 }
