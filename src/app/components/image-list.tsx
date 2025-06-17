4b78c122249cf0b192a05ea67adcac844a9d7f4a
mefarran2 <mefarran2@vt.edu>
2025-06-16 22:02:07 -0400

    home page changes

---
+++ src/app/components/image-list.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+export default function StandardImageList() {
+  return (
+    <ImageList sx={{ width: 650, height: 500}} cols={3} rowHeight={164} >
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
+
+const itemData = [
+  {
+    img: '/pics/n1.png',
+    title: 'Breakfast',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: '/pics/n2.png',
+    title: 'Burger',
+  },
+  {
+    img: '/pics/n3.png',
+    title: 'Camera',
+  },
+  {
+    img: '/pics/n4.png',
+    title: 'Coffee',
+    cols: 2,
+  },
+  {
+    img: '/pics/n5.png',
+    title: 'Hats',
+    cols: 2,
+  },
+  {
+    img: '/pics/n6.png',
+    title: 'Honey',
+    author: '@arwinneil',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: '/pics/n7.png',
+    title: 'Basketball',
+  },
+  {
+    img: '/pics/n8.png',
+    title: 'Fern',
+  },
+  {
+    img: '/pics/n9.png',
+    title: 'Mushrooms',
+    rows: 2,
+    cols: 2,
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,37 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import QuiltedImageList from './components/image-list'
 
 
 export default function Home() {
   return (
     <Container maxWidth="xl">
-      <Box marginTop={5}>
+      <Box marginTop={4}>
         <Grid container spacing={5}>
           <Grid size={6} >
-            <Paper elevation={4} sx={{height: 500}}>
-              <Typography sx={{fill: 'true'}}>paper</Typography>
+            <Paper elevation={4} sx={{height: '125%', backgroundColor: '#f5f4f0'}}>
+                <Box justifyContent={'center'} alignItems="center" display="flex" marginBottom={5}>
+              <QuiltedImageList/>
+              </Box>
             </Paper>
           </Grid>
           <Grid size={6} >
-            <Paper elevation={4}>
-              <Typography>paper</Typography>
+            <Paper elevation={4} sx={{height: '125%', backgroundColor: '#f5f4f0'}}>
+              <Box justifyContent={'center'} alignItems="center" display="flex" marginBottom={5}>
+                <Stack>
+                  <Typography sx={{textAlign: 'center'}}>Been here before? Log in here: </Typography>
+                  <Button variant="outlined" sx={{justifyContent: 'center'}}>Log in</Button>
+                </Stack>
+              </Box>
+              <Box justifyContent={'center'} alignItems="center" display="flex">
+                <Stack>
+                  <Typography sx={{textAlign: 'center'}}>New here? Sign up for an account: </Typography>
+                  <Button variant="outlined" sx={{justifyContent: 'center'}}>Sign up</Button>
+                </Stack>
+              </Box>
             </Paper>
           </Grid>
         </Grid>
