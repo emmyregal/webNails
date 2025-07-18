ac63c9ce9bfa42743161582f5a79fb5d1e355c26
Emmy Regal <emmyregal73@vt.edu>
2025-07-17 21:58:01 -0700

    grid changes

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -68,12 +68,11 @@ export function PriceCard2() {
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
           <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-            Word of the Day
           </Typography>
           <Typography variant="h5" component="div">
-            be{bull}nev{bull}o{bull}lent
+            medium length
           </Typography>
-          <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+          <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>$65</Typography>
           <Typography variant="body2">
             well meaning and kindly.
             <br />
