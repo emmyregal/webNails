72cef26a5aca29aa7627d541cb5495fbacb06a60
mefarran2 <mefarran2@vt.edu>
2025-08-26 10:28:01 -0400

    booking page working on date time picker

--- src/app/book-now/page.tsx
+++ src/app/book-now/page.tsx
@@ -17,6 +17,14 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import Chip from '@mui/material/Chip';
+
+
 //todo: 
 // input validation
 // submitting actually submits & sends to db
@@ -28,7 +36,7 @@ type NailType = 'acrylic' | 'gel';
 export default function Booking() {
     // const [selectedType, setSelectedType] = useState<NailType | "">('');
     const [selectedType, setSelectedType] = useState('');
-
+    const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
     const [inputValid, setInputValid] = useState<boolean>(false);
 
 
@@ -47,6 +55,7 @@ export default function Booking() {
         // setOpen(false);
     };
 
+
     // const handleChange = (event: SelectChangeEvent) => {
     //     const value = event.target.value as NailType;
     //     setSelectedType(value);
@@ -61,7 +70,13 @@ export default function Booking() {
         }
     }
 
-    const typeDurations = {
+    interface durations {
+        [key: string]: number;
+        acrylic: number
+        gel: number
+    }
+
+    const typeDurations: durations = {
         acrylic: 2,  //2 hour time period for acrylic set 
         gel: 1.5, // 1.5 hour time slot for gel set 
     };
@@ -70,6 +85,46 @@ export default function Booking() {
     const handleTypeChange = (event: SelectChangeEvent) => {
         setSelectedType(event.target.value as string);
     };
+
+    const nineAM = dayjs().set('hour', 9).startOf('hour');
+    const sevenPM = dayjs().set('hour', 19).startOf('hour');
+
+    const renderCalandar = () => {
+        if (selectedType.length == 0) {
+            return (
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DemoContainer components={['DateTimePicker']}>
+                        <DateTimePicker
+                            disabled
+                            label={`Choose type of nails before date & time`}
+                            value={selectedDate}
+                        />
+
+                    </DemoContainer>
+                </LocalizationProvider>
+            )
+        } else {
+            return (
+                <div>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DemoContainer components={['DateTimePicker']}>
+                        <DateTimePicker
+                            label={`Choose date & time (duration: ${typeDurations[selectedType]} hr)`}
+                            minTime={nineAM}
+                            maxTime={sevenPM}
+                            onChange={(newValue) => setSelectedDate(newValue)}
+                            value={selectedDate}
+                            name='Date'
+                        // disablePast
+                        />
+                    </DemoContainer>
+                </LocalizationProvider>
+                {/* display the date time in a nice way: */}
+                {/* <Chip label={`Chosen Date & Time:`}  /> */} 
+                </div>
+            )
+        }
+    }
 
 
     return (
@@ -81,14 +136,16 @@ export default function Booking() {
 
             <Box>
                 <Stack direction={'column'} spacing={4} display={'flex'} >
-                    <Box maxWidth={400}>
-                        <Typography className='header'>What type of nails?</Typography>
-                        <Typography className='subHeader'>
-                            Enter the type of nails you are looking for here
-                        </Typography>
-                        {/* <TypeSelect onChange={handleTypeChange} /> */}
-                        <Box sx={{ minWidth: 120 }}>
-                            <form onSubmit={handleSubmit} id="booking-form">
+                    <form onSubmit={handleSubmit} id="booking-form">
+                        <Box maxWidth={400}>
+                            <Typography className='header'>What type of nails?</Typography>
+                            <Typography className='subHeader'>
+                                Enter the type of nails you are looking for here
+                            </Typography>
+                            {/* <TypeSelect onChange={handleTypeChange} /> */}
+
+                            <Box sx={{ minWidth: 120 }}>
+
                                 <FormControl fullWidth>
                                     <InputLabel sx={{
                                         color: inputValid ? '#d32f2f' : 'rgba(0, 0, 0, 0.6)',
@@ -111,17 +168,20 @@ export default function Booking() {
                                     </Select>
                                     {inputValid && <FormHelperText sx={{ color: '#d32f2f' }}>This field is required</FormHelperText>}
                                 </FormControl>
-                            </form>
+
+                            </Box>
                         </Box>
-                    </Box>
-                    <Box maxWidth={400}>
-                        <Typography className='header'>When will your appointment be?</Typography>
-                        <Typography className='subHeader'>
-                            Enter your appointment date and time here
-                        </Typography>
-                        {/* <Calandar/> */}
-                        {/* <Calandar duration={typeDurations[selectedType]} /> */}
-                    </Box>
+                        <Box maxWidth={400}>
+                            <Typography className='header'>When will your appointment be?</Typography>
+                            <Typography className='subHeader'>
+                                Enter your appointment date and time here
+                            </Typography>
+                            {/* <Calandar/> */}
+                            {/* <Calandar duration={typeDurations[selectedType]} /> */}
+
+                            {renderCalandar()}
+                        </Box>
+                    
 
                     <Box maxWidth={400}>
                         <Typography className='header'>Additional comments</Typography>
@@ -134,6 +194,7 @@ export default function Booking() {
                             multiline
                             maxRows={8}
                             variant="filled"
+                            name="comments"
                             fullWidth
                         />
                     </Box>
@@ -150,6 +211,7 @@ export default function Booking() {
                             <input
                                 type="file"
                                 accept="image/*"
+                                name="inspo pics"
                                 hidden
                                 multiple // mult pics if needed
                                 onChange={(e) => {
@@ -160,6 +222,7 @@ export default function Booking() {
                             />
                         </Button>
                     </Box>
+                    </form>
                 </Stack>
                 <Divider sx={{ color: alpha(`${theme.palette.secondary.main}`, 0.5), mt: 2, mb: 4 }} />
                 <Box maxWidth={400}>
